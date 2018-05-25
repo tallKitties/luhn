d7883c83badf7bc29a7b3bcb6c761a33bf77a906
tallKitties <bkautt73@gmail.com>
2018-05-25 01:15:15 +0000

    removed instance variables

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -1,20 +1,19 @@
 module Luhn
   def self.is_valid?(number)
-    @card_number = number
-    @card_array = separate_digits
-    @every_other_doubled = double_every_other
-    divisible_by_10?
+    divisible_by_10?(number)
   end
 
   private
 
-  def self.divisible_by_10?
-    total = @every_other_doubled.reduce(&:+)
+  def self.divisible_by_10?(number)
+    total = double_every_other(number).reduce(&:+)
     total % 10 == 0
   end
 
-  def self.double_every_other
-    @card_array.reverse.map.with_index do |digit, i|
+  def self.double_every_other(number)
+    card_array = separate_digits(number)
+
+    card_array.reverse.map.with_index do |digit, i|
       if i.odd?
         digit = digit * 2
         digit > 9 ? digit - 9 : digit
@@ -24,7 +23,7 @@ module Luhn
     end
   end
 
-  def self.separate_digits
-    @card_number.to_s.split('').map(&:to_i)
+  def self.separate_digits(number)
+    number.to_s.split('').map(&:to_i)
   end
 end
